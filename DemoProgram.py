8227db9e4e67b3eee0245b44fae27687eb78bbc9
Robbie Jordan <60371724+GuardingDark@users.noreply.github.com>
2021-10-19 20:56:39 -0600

    Create DemoProgram.py

---
+++ DemoProgram.py
@@ -0,0 +1,12 @@
+import TwinPrimes as tp
+
+tp.GenerateHexas(20)
+tp.FindInvalidChains(5)
+tp.GenerateCombo(4, 28)
+tp.ViewCombo(4, 20, 8)
+tp.ViewCritCombos(5)
+tp.FindAverageGap(20)
+tp.ValidCoordinates(8)
+tp.GenerateCombos(4)
+tp.ValidNumApproximation(20)
+tp.ViewCritArea()
