095d5ebfb557d5e1417e5d3878e770c4b911820b
Freya <freyakgray@gmail.com>
2021-10-28 11:13:48 -0600

    Update TwinPrimesGPU.py

--- TwinPrimesGPU.py
+++ TwinPrimesGPU.py
@@ -1,14 +1,29 @@
 import numpy as np
-from numba import njit, prange, cuda
+from numba import njit, prange, cuda, jit
 import TwinPrimes as tp
 import time
 
-hexaArray = np.empty(100000, dtype = int)
-sextandsArray = np.empty(100000, dtype = int)
-squareSextandsArray = np.empty(100000, dtype = int)
-hexaArrayGPU = np.empty(100000, dtype = int)
-sextandsArrayGPU = np.empty(100000, dtype = int)
-squareSextandsArrayGPU = np.empty(100000, dtype = int)
+hexaArrayGPU = np.empty(100000000, dtype = int)
+sextandsArrayGPU = np.empty(100000000, dtype = int)
+squareSextandsArrayGPU = np.empty(100000000, dtype = int)
+hexaArray = np.empty(100000000, dtype = int)
+sextandsArray = np.empty(100000000, dtype = int)
+squareSextandsArray = np.empty(100000000, dtype = int)
+hexasList = []
+sextandsList = []
+squareSextandsList = []
+
+
+@cuda.jit
+def GenerateHexasGPU(hexas, hexaArrayGPU, sextandsArrayGPU, squareSextandsArrayGPU):
+    for i in prange(hexas):
+        pos = cuda.grid(1)
+        hexaArrayGPU[pos] = 3*(pos + 1) + (3/2) - ((-1)**(pos + 1) * (1/2))
+        sextandsArrayGPU[pos] = ((1/2) * (pos+1)) + (1/4) + ((1/4) * (-1)**((pos+1) - 1))
+        if i % 2 == 0:
+            squareSextandsArrayGPU[pos] = 6*(sextandsArrayGPU[pos]**2) - 2*sextandsArrayGPU[pos]
+        else:
+            squareSextandsArrayGPU[pos] = 6*(sextandsArrayGPU[pos]**2) + 2*sextandsArrayGPU[pos] 
 
 @njit(fastmath = True)
 def GenerateHexasPara(hexas, hexaArray, sextandsArray, squareSextandsArray):
@@ -20,38 +35,32 @@ def GenerateHexasPara(hexas, hexaArray, sextandsArray, squareSextandsArray):
         else:
             squareSextandsArray[i] = 6*(sextandsArray[i]**2) + 2*sextandsArray[i] 
 
-@cuda.jit
-def GenerateHexasGPU(hexas, hexaArray, sextandsArray, squareSextandsArray):
-    for i in prange(hexas):
-        hexaArray[i] = 3*(i + 1) + (3/2) - ((-1)**(i + 1) * (1/2))
-        sextandsArray[i] = ((1/2) * (i+1)) + (1/4) + ((1/4) * (-1)**((i+1) - 1))
-        if i % 2 == 0:
-            squareSextandsArray[i] = 6*(sextandsArray[i]**2) - 2*sextandsArray[i]
-        else:
-            squareSextandsArray[i] = 6*(sextandsArray[i]**2) + 2*sextandsArray[i] 
 
+
+threads_per_block = 32
+blocks_per_grid = (hexaArrayGPU.size +(threads_per_block -1))
+#blocks_per_grid = (hexaArrayGPU.shape[0]/threads_per_block)
+d_HexaArray = cuda.to_device(hexaArrayGPU)
+d_SextandsArray = cuda.to_device(sextandsArrayGPU)
+d_SextandsArray = cuda.to_device(squareSextandsArrayGPU)
+
+print('Generate Hexas GPU for ' + str(hexaArrayGPU.size) + ' hexas')
 start = time.time()
-GenerateHexasPara(100000, hexaArray, sextandsArray, squareSextandsArray)
+GenerateHexasGPU[blocks_per_grid, threads_per_block](100000000, d_HexaArray,d_SextandsArray, d_SextandsArray )
 end = time.time()
-print("Elapsed (with compilation) = %s" % (end - start))
+print("GPU time elapsed = %s" % (end - start))
+#gpuArray = d_ary.copy_to_host()
+#print(gpuArray)
 
+print('Generate Hexas Numba for ' + str(hexaArrayGPU.size) + ' hexas')
 start = time.time()
-GenerateHexasPara(100000, hexaArray, sextandsArray, squareSextandsArray)
+GenerateHexasPara(100000000, hexaArray, sextandsArray, squareSextandsArray)
 end = time.time()
-print("para compElapsed (with compilation) = %s" % (end - start))
+print("Numba time elapsed = %s" % (end - start))
 
-#start = time.time()
-#GenerateHexasGPU(100000, hexaArrayGPU, sextandsArrayGPU, squareSextandsArrayGPU)
-#end = time.time()
-#print("GPU Elapsed (with compilation) = %s" % (end - start))
-
-#start = time.time()
-#GenerateHexasGPU(100000, hexaArrayGPU, sextandsArrayGPU, squareSextandsArrayGPU)
-#end = time.time()
-#print("GPU comp para compElapsed (with compilation) = %s" % (end - start))
-
+print('Generate Hexas normal for ' + str(hexaArrayGPU.size) + ' hexas')
 start = time.time()
-tp.GenerateHexas(100000)
+tp.GenerateHexas(100000000)
 end = time.time()
-print("norm Elapsed (with compilation) = %s" % (end - start))
+print("normal time elapsed = %s" % (end - start))
 
