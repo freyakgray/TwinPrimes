0a22e1997dae1d45eec8d83fef6cecc598d53acc
lucas-cell <lucasnieddu@gmail.com>
2021-11-03 23:53:14 -0600

    Add FastTwinPrimes.py and Compare.py

--- Compare.py
+++ Compare.py
@@ -4,27 +4,65 @@ import time
 from numba import jit
 import pandas as pd
 
-size = 1000
+
+# Global Variables 
+size = 100000
+hexas_num = 8
+index = 28
+hexas_checked = 10
+start = 20
+length = 4
 
 
-start_tp = time.time()
-tp.GenerateHexas(size)
-tp.FindInvalidChains(8)
-tp.GenerateCombo(7, 28)
-tp.ViewCombo(4, 20, 4)
-end_tp = time.time()
-elapsed_tp = end_tp - start_tp
-print("Lists: ", elapsed_tp)
+def run_twin_primes():
+    start_tp = time.time()  
+    tp.GenerateHexas(size)
+    tp.FindInvalidChains(hexas_num)
+    tp.GenerateCombo(hexas_num, index)
+    tp.ViewCombo(hexas_checked, start, length)
+    tp.ViewCritCombos(hexas_checked)
+    tp.FindAverageGap(hexas_checked)
+    tp.ValidCoordinates(hexas_num)
+    tp.GenerateCombos(hexas_num)
+    tp.ValidNumApproximation(hexas_num)
+    end_tp = time.time()
+    elapsed_tp = end_tp - start_tp
+    print("List: " + str(elapsed_tp))
 
-start_ftp = time.time()
-ftp.GenerateHexas(size)
-ftp.FindInvalidChains(8)
-ftp.GenerateCombo(7, 28)
-ftp.ViewCombo(4, 20, 4)
-end_ftp = time.time()
-elapsed_ftp = end_ftp - start_ftp
-print("numba.jit: ", elapsed_ftp)
+def run_fast_twin_primes():
+    start_ftp = time.time()
+    ftp.GenerateHexas(size)
+    ftp.FindInvalidChains(hexas_num)
+    ftp.GenerateCombo(hexas_num, index)
+    ftp.ViewCombo(hexas_checked, start, length)
+    ftp.ViewCritCombos(hexas_checked)
+    ftp.FindAverageGap(hexas_checked)
+    ftp.ValidCoordinates(hexas_num)
+    ftp.GenerateCombos(hexas_num)
+    ftp.ValidNumApproximation(hexas_num)
+    end_ftp = time.time()
+    elapsed_ftp = end_ftp - start_ftp
+    print("numba.jit: " + str(elapsed_ftp))
+
+def best():
+    size_req = (size >= 10000000 or hexas_num >= 6 or size >= 10000000)
+    if(size_req):
+        return run_fast_twin_primes()
+    else:
+        return run_twin_primes()
 
 
+def compare_both():
+    run_fast_twin_primes()
+    run_twin_primes()
+
+print()
+print("Size: ", size)
+print("Number of Hexas = ", hexas_num)
+compare_both()
+print()
+print("Faster choice: ")
+best()
+    
 
 
--- FastTwinPrimes.py
+++ FastTwinPrimes.py
@@ -1,13 +1,11 @@
 from logging import critical
 from numba.np.ufunc import parallel
 import numpy as np
-import numba 
 from numba import jit
 import warnings
+import os
+from numpy.core.fromnumeric import size
 warnings.filterwarnings('ignore')
-
-
-#size = int(input("Enter size of your own choosing to intialize arrays: "))
 
 @jit(nopython = True, parallel = True)
 def InitializeArrays(size):
@@ -17,7 +15,7 @@ def InitializeArrays(size):
     square_sextands_array = np.empty(size)
     return hexas_array, sextands_array, square_sextands_array
 
-@jit(nopython = True, parallel = True)
+@jit(nopython = True, parallel = True, fastmath = True)
 def GenerateArrays(size):
     hexas_array, sextands_array, square_sextands_array = InitializeArrays(size)
     sxtnd = 0
@@ -32,7 +30,6 @@ def GenerateArrays(size):
         square_sextands_array[i] =  (((hexas_array[i] * hexas_array[i]) - 1) / 6)
         sextands_array[i] =  sxtnd
     return hexas_array, sextands_array, square_sextands_array
-
 
 @jit(fastmath = True)
 def GenerateHexas(hexas_num):
@@ -110,7 +107,7 @@ def FindInvalidChains(n):
     critical_zone = square_sextands_array[n - 1] - square_sextands_array[n - 2]
     return max_chain, max_length, critical_zone
 
-@jit(fastmath = True)
+@jit(nopython = True, parallel = True, fastmath = True)
 def GenerateCombo(hexas_num, index):
     """
     INPUT: 
@@ -169,3 +166,213 @@ def ViewCombo(hexas_checked, start, length):
         combos_list.insert(i, combo)
     combos_array = np.array(combos_list)
     return combos_array
+
+@jit(fastmath = True)
+def ViewCritCombos(hexas_checked):
+    """INPUT: 
+    hexasChecked: The number of hexas to be checked 
+    OUTPUTS: 
+    displays the combos in the critical area
+    RETURNS:
+    combosList: list of combos
+    """
+    hexas_array, sextands_array, square_sextands_array = GenerateArrays(hexas_checked)
+    start = 1
+    length = int(square_sextands_array[hexas_checked - 1] + 1)
+    combos_array = ViewCombo(hexas_checked, start, length)
+    return combos_array
+
+@jit(nopython = True, parallel = True, fastmath = True)
+def FindAverageGap(hexas_checked):
+    """INPUTS: 
+    hexasChecked: The number of hexas to be checked 
+    OUTPUTS: 
+    Displays the expected average gap between valid combos (hexorial / Lexorial)
+    Returns:
+    num: Hexorial of n hexas
+    denom: Lexorial of n hexas
+    gap: average of hexorial/lexorial
+    NOTES: May want to take an input and find the average gap in that range (e.g. if n = 2, find the average gap in [0, (5*7)) range
+    """
+    hexas_array, sextands_array, square_sextands_array = GenerateArrays(hexas_checked)
+    gap, num, denom = 1,1,1
+    for i in range(0,hexas_checked):
+        num *= hexas_array[i]
+        denom *= hexas_array[i]-2
+    gap = num/denom
+    return num, denom, gap
+
+@jit(fastmath = True)
+def ValidCoordinates(hexas_num):
+    """INPUTS:
+    hexasNum: The number of hexas being examined
+    OUTPUT: .txt document given the coordinates where x is the number of hexas checked and y is the number of valid combos in critical area
+    """
+    combo = ""
+    validNum = 0
+    combos_list = []
+
+    hexas_array, sextands_array, square_sextands_array = GenerateArrays(hexas_num)
+
+    for i in range(2, hexas_num + 1): # Cycle through all hexa pairs
+        combo = "(" + str(i) + ","
+        # print(combo)
+        validNum = 0
+
+        for j in range(int(square_sextands_array[i-2]), int(square_sextands_array[i-1] + 1)):
+            valid = True
+            for k in range(i):
+                result1 = ((6 * j) % hexas_array[k] == 1)
+                result2 = (6 * j) % hexas_array[k] == hexas_array[k] - 1
+                if(result1 or result2):
+                    valid = False
+
+            if(valid):
+                validNum += 1
+
+        combo += str(validNum) + ")"
+        combos_list.append(combo)
+    
+    WriteValidCoordinates(combos_list, hexas_num)
+        
+
+def WriteValidCoordinates(combos_list, hexas_num):
+
+    hexas_checked = "Hexas checked: " + str(hexas_num) + "\n"
+    file_name = "valid_coordinates.txt"
+
+    if os.path.exists(file_name):
+        os.remove(file_name)
+
+    with open(file_name, "a") as file:
+        file.write(hexas_checked)
+
+        for i in combos_list:
+            file.write(i)
+            file.write("\n")
+
+@jit(fastmath = True)
+def GenerateCombos(hexas_num):
+    """INPUTS:
+    hexasNum: The number of hexas being examined (e.g. if hexasnum = 3, then 5,7, and 11 are being examined)
+    OUTPUTS: Combines functionality of findInvalidChains() and viewChains()
+    """
+    # boolean variables
+    adv = True 
+    valid = True
+
+    # combos_array initialized with size of hexasNum
+    combos_array = np.empty(hexas_num)
+    hexas_array, sextands_array, square_sextands_array = GenerateArrays(hexas_num)
+
+    # integer varaibles
+    index = 1
+    invalidChainLength = 0
+    maxChainLength = 0
+    invalidStart = 0
+
+    # float variables 
+    chainLengthSum = 0.0
+    chainsNum = 0.0
+
+    # information list
+    info_list = []
+
+    while (adv):
+        adv = False
+        valid = True
+
+        print_statement = str(str(index) + ": ")
+        for i in range(0 , len(combos_array)):
+            combos_array[i] = (index % hexas_array[i])
+
+            if(combos_array[i] != 0):
+                adv = True
+            
+            if(i % 2 == 0):
+                result1 = combos_array[i] == ((i + 2) / 2) 
+                result2 = combos_array[i] == hexas_array[i] - ((i + 2) / 2)
+                if (result1) or (result2):
+                    valid = False
+
+            elif(i % 2 == 1):
+                result3 = combos_array[i] == ((i + 1) / 2)
+                result4 = combos_array[i] == hexas_array[i] - ((i + 1) / 2)
+                if (result3) or (result4):
+                    valid = False
+            
+            print_statement += " "
+            print_statement += str(combos_array[i])
+
+        if(valid):
+            print_statement += " <"
+            chainLengthSum += invalidChainLength
+            chainsNum+=1
+            invalidChainLength = 0
+        else:
+            invalidChainLength +=1 
+
+            if(invalidChainLength > maxChainLength):
+                maxChainLength = invalidChainLength
+                invalidStart = index - maxChainLength + 1
+
+        index+=1
+        
+    return print_statement, maxChainLength, invalidStart
+
+@jit(fastmath = True)
+def ValidNumApproximation(hexas_num):
+    """INPUTS: hexasNum
+    OUTPUTS: Calculates an approximated for the expected number of valid combos within the domain (i.e. [1, A] where A is the upper bound of
+   the critical area), counts the true number of valid combos within the domain, and displays the error between them. Outputs to a txt file named
+   valid_num_approx.txt as well as the console. 
+    """
+
+    '''
+        prodApprox is the approximated number of valid combos by taking quotient of the lesser hexorial and the true hexorial,
+		then multiplying that by the index, which in this case is the square-sextand of the greatest hexa being examined, i.e.
+		the upper boundary of the critical area
+
+        prodTrue is the actual number of valid combos counted within the range of [1, A], where A is the upper boundary of the
+		critical area
+    '''
+    # Generate arrays  
+    hexas_array, sextands_array, square_sextands_array = GenerateArrays(hexas_num + 1)
+
+    # integers
+    endPoint = int(square_sextands_array[hexas_num - 1])
+    marker = 1
+
+    # floats
+    prodTrue = 0.0
+    prodApprox = 1.0
+
+    # boolean values
+    validCombo = False
+
+    for i in range(1, endPoint):
+        validCombo = True
+        if(i > square_sextands_array[marker]):
+            marker += 2
+
+        for j in range(0, marker):
+            result1 = i % hexas_array[j] == sextands_array[j]
+            result2 = i % hexas_array[j] == (hexas_array[j] - sextands_array[j])
+            if (result1) or (result2):
+                validCombo = False
+                break
+        
+        if(validCombo == True):
+            prodTrue+=1
+            print_statement = "Valid at " + str(i)
+                
+    for j in range(0, hexas_num):
+        prodApprox *= ((float(hexas_array[j] - 2)) / (float(hexas_array[j])))
+    
+    prodApprox *= endPoint
+    error = (prodTrue - prodApprox) / float(prodTrue)
+    error_percentage = error * 100
+
+    return print_statement, endPoint, prodApprox, prodTrue, error_percentage
+     
+    
